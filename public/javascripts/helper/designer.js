5b1af1389932e80a30b73db75518b536e9fa8bb0
lobbychmd <lobbychmd@gmail.com>
2012-07-05 09:54:25 +0800

    3

---
+++ public/javascripts/helper/designer.js
@@ -0,0 +1,17 @@
+﻿$.codeMirrors = {};
+
+    $.fn.toggleEditor_code = function (show) {
+        return this.each(function () {
+            if (show) $.codeMirrors[$(this).attr("name")] = CodeMirror.fromTextArea(this, {
+                lineNumbers: true,
+                matchBrackets: true,
+                mode: $(this).attr('scriptType')//"text/x-plsql"
+            });
+            else {
+                if ($.codeMirrors[$(this).attr("name")])
+                    $.codeMirrors[$(this).attr("name")].toTextArea();
+                else alert("codemirror " + $(this).attr("name") + " not found.");
+            }
+        });
+    }
+    
