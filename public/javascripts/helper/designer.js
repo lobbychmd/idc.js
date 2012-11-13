3216072f80b76f3a7e01df8725e51e11fff3011a
lobbychmd <lobbychmd@gmail.com>
2012-11-13 18:25:49 +0800

    asdf

--- public/javascripts/helper/designer.js
+++ public/javascripts/helper/designer.js
@@ -1,23 +1,23 @@
 ﻿$.codeMirrors = {};
 
-    $.fn.toggleEditor_code = function (show) {
-        return this.each(function () {
-            if (show) {
-                var width = $(this).width();
-                $.codeMirrors[$(this).attr("name")] = CodeMirror.fromTextArea(this, {
-                    lineNumbers: true,
-                    matchBrackets: true,
-                    mode: $(this).attr('scriptType')//"text/x-plsql"
-                });
-                $(this).parent().find('.CodeMirror>.CodeMirror-scroll').width(width + "px");
-            }
-            else {
-                if ($.codeMirrors[$(this).attr("name")])
-                    $.codeMirrors[$(this).attr("name")].toTextArea();
-                else alert("codemirror " + $(this).attr("name") + " not found.");
-            }
-        });
-    }
+        $.fn.toggleEditor_code = function (show) {
+            return this.each(function () {
+                if (show) {
+                    var width = $(this).width();
+                    $.codeMirrors[$(this).attr("name")] = CodeMirror.fromTextArea(this, {
+                        lineNumbers: true,
+                        matchBrackets: true,
+                        mode: $(this).attr('scriptType')//"text/x-plsql"
+                    });
+                    $(this).parent().find('.CodeMirror>.CodeMirror-scroll').width(width + "px");
+                }
+                else {
+                    if ($.codeMirrors[$(this).attr("name")])
+                        $.codeMirrors[$(this).attr("name")].toTextArea();
+                    else alert("codemirror " + $(this).attr("name") + " not found.");
+                }
+            });
+        }
     
 
     $.fn.toggleEditor_JSON = function (show) {
@@ -36,5 +36,17 @@
             else $(this).show();
 
             return true;
+        });
+    }
+
+    $.fn.toggleEditor_visual = function (show) {
+        return this.each(function () {
+            if (show) {
+                $('#tp_visual_uidesigner').tmpl({}).insertBefore(this);
+                $(this).hide();
+            }
+            else {
+                alert(1);
+            }
         });
     }
