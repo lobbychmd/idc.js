8103e567425d8b865732a6143f2d2d97da659e67
lobbychmd <lobbychmd@gmail.com>
2013-05-06 18:54:01 +0800

    sqlbulider support bit check

--- public/javascripts/helper/sqlHelper.js
+++ public/javascripts/helper/sqlHelper.js
@@ -7,9 +7,25 @@ sqlHelper.prototype = {
     lock: function () {
         return _.find(this.meta.Columns, function (i) { return i.ColumnName == "dLastUpdateTime"; }) ? "and \n  DATEDIFF(ms, :Old_LastUpdateTime, LastUpdateTime) between 0 and 1000" : "";
     },
+    sel_field: function (fieldName) {
+        var metaf = _.find(this.meta.Columns, function (i) { return i.ColumnName == fieldName; });
+        if (metaf.MetaType == "CheckTag") {
+            var selection = _.map(metaf.Selection.split(';'), function (i) { var ii = i.split("."); return {key: parseInt(ii[0]), value:ii[1]} });
+            var last = _.last(selection);
+            var statement = _.map( selection, function(i){
+                var lasti = last.key == i.key;
+                var jj = Math.pow(2, i.key).toString();
+                return "Checked" + (lasti?"": "_" + i.key) + " = Convert(bit, case when " + fieldName + " & " + jj + " = " + jj + " then 1 else 0 end)";
+            }).join(",\n");
+            return statement;
+        }
+
+        else return fieldName.substring(1) + " = " + fieldName;
+    },
     select: function (fields) {
+        var o = this;
         return "select \n" +
-                $.map(fields, function (value, index) { return "  " + value.substring(1) + " = " + value; }).join(", \n")
+                $.map(fields, function (value, index) { return "  " + o.sel_field(value); }).join(", \n")
                 + "\n  from " + this.meta.TableName +
                 "\n where \n" +
                 $.map(
@@ -42,11 +58,11 @@ sqlHelper.prototype = {
                     function (value, index) { return "  " + value.ColumnName + " = :" + value.ColumnName.substring(1); }).join(", \n");
     },
     IUD: function (fields) {
-        return "if :UpdateFlag = 'I'\n" + 
-        _.map( this.insert(fields).split("\n"), function(i){return "  " + i;}).join("\n") + 
-            "\nelse if :UpdateFlag = 'U'\n" + 
-            _.map( this.u(fields).split("\n"), function(i){return "  " + i;}).join("\n") + 
-            "\nelse if :UpdateFlag = 'D'\n" +  
-            _.map( this.d(fields).split("\n"), function(i){return "  " + i;}).join("\n") ;
+        return "if :UpdateFlag = 'I'\n" +
+        _.map(this.insert(fields).split("\n"), function (i) { return "  " + i; }).join("\n") +
+            "\nelse if :UpdateFlag = 'U'\n" +
+            _.map(this.u(fields).split("\n"), function (i) { return "  " + i; }).join("\n") +
+            "\nelse if :UpdateFlag = 'D'\n" +
+            _.map(this.d(fields).split("\n"), function (i) { return "  " + i; }).join("\n");
     }
 }
